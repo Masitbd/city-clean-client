432181c1ecab93345b7ed2ccb7d0efc5766bfe25
sarker24 <sarker24@gmail.com>
2023-11-15 00:57:36 +0100

    change link

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -5,8 +5,8 @@ import { getFromLocalStorage } from "@/utils/local-storage";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    //baseUrl: "https://clean-co-backend-r545kc87n-riyaadhossain.vercel.app/api/v1",
-    baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: "https://city-clean-aps-masitbd.vercel.app/api/v1",
+    // baseUrl: "http://localhost:5000/api/v1",
     prepareHeaders: (headers, { getState }) => {
       const accessToken = getFromLocalStorage("accessToken");
       if (accessToken) {
